acd6bd2d7846bc51495fcb45eb618a64f88da209
Katie Palof <katie.palof@alaska.gov>
2018-06-04 15:40:55 -0800

    bring in all pot summary files at once.

--- code/bycatch_code.R
+++ code/bycatch_code.R
@@ -13,10 +13,13 @@ source('./code/packages.R')
 # The data is accessed via: Data Access – Shellfish – Biological Data – Crab Observer – Reports & Info
 # Fish ticket data is from Ben Daly - need to ask him where this is stored.
 
-# for one fishery  QO16 currently
-# Species Composition Reports - Sample pot summary - Fishery: QO16 - Species: snow crab
-sampled_pots <- read.csv('data/potSummary_QO16.csv') 
-# 
+# This is for one species - snow crab. Look at all open fishery in that year (2017)
+# Species Composition Reports - Sample pot summary - Fishery: QO16/ - Species: snow crab
+files_pots <- dir('data/pots', pattern = '*.csv')
+sampled_pots <- files_pots %>% 
+                  map(function(x) read_csv(file.path('data/pots', x))) %>% 
+                  reduce(rbind) 
+# From Ben D, need to figure out where this comes from **fix**
 fish_tkt <- read.xlsx("data/FishTicketsummaries 2016-17.xlsx", sheetName = 'QO16', startRow = 3, 
                       endRow = 53)
 # Data dumps - Crab Detail Data - Fishery: QO16 - Species: snow crab - Sex: all 
