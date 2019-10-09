0d0767a27f31fb76accd0cef9670382be4e4065d
kjpalof <katie.palof@alaska.gov>
2019-10-09 15:37:51 -0800

    through observer data cpue summary

--- code/BBRKC discard estimates_kjp.R
+++ code/BBRKC discard estimates_kjp.R
@@ -53,3 +53,52 @@ obs_dump %>%
          species == "red king crab",
          sex %in% c(1, 2)) -> obs_dump2
 
+# create mean weight data frame, add years with fishery closures
+obs_dump2 %>%
+  group_by(year) %>%
+  summarize(female_wt = mean(wt_lb[sex == 2], na.rm = T),
+            male_wt = mean(wt_lb[sex == 1], na.rm = T),
+            sublegal_male_wt = mean(wt_lb[sex == 1 & legal ==0], na.rm = T),
+            lnr_male_wt = mean(wt_lb[sex == 1 & (legal == 2 | (year < 1998 & legal == 1))], na.rm = T),
+            legal_male_wt = mean(wt_lb[sex == 1 & legal %in% c(1 ,2)], na.rm=T)) %>%
+  add_row(year = 1994:1995,.before = 4) -> mean_wt
+
+# # create fields to parse data
+# sp_fish - species
+# dir_cr - fishery area/type?
+# year
+# tot_male - total males (sublegal and legal)
+# only retain males and females in Bristol Bay fisheries
+potsum %>% 
+  mutate(year = as.numeric(substring(fishery, 3, 4)),
+       year = ifelse(year <= 75, year + 2000, year + 1900),
+       target = substring(fishery, 2, 2), 
+       fishery_type = substring(fishery, 1, 1)) %>% 
+  left_join(metadata) %>% 
+  left_join(metadata2) %>% 
+  mutate(dir_cr = case_when(fishery_type == "X" ~ "CR",
+                            fishery_type == "Q" ~ "NA", 
+                            fishery_type %in% c("C", "E", "T") ~ "directed"), 
+         tot_male = sublegal + tot_legal) %>% 
+  filter(dir_cr != "NA",
+         species == "red king crab") -> potsum2
+
+# create observer cpue data frame, add years with fishery closures
+potsum2 %>%
+  group_by(year) %>%
+  summarize(obs_num_fem = sum(female, na.rm = T),
+            obs_num_male = sum(tot_male, na.rm = T), 
+            obs_num_sublegal_male = sum(sublegal, na.rm = T), 
+            obs_num_lnr_male = sum(legal_nr, na.rm = T),
+            obs_num_legal_male = sum(tot_legal, na.rm = T), 
+            obs_effort = n()) %>%
+  as.data.frame() %>%
+  mutate(obs_fem_cpue = obs_num_fem / obs_effort,
+         obs_male_cpue = obs_num_male / obs_effort,
+         obs_sublegal_male_cpue = obs_num_sublegal_male / obs_effort,
+         obs_lnr_male_cpue = obs_num_lnr_male / obs_effort,
+         obs_legal_male_cpue = obs_num_legal_male / obs_effort) %>%
+  select(1,2,8,3,9,4,10,5,11,6,12) %>%
+  add_row(year = 1994:1995, .before = 4) -> obs_cpue
+
+
